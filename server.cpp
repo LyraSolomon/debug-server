d4a20b39033a566ec4434d64631fcbfd3cea67b3
steampunc <finnegan.mccool@gmail.com>
2015-11-19 20:26:15 -0800

    got connection working in c+=, working on porting over

---
+++ server.cpp
@@ -0,0 +1,129 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <fstream>
+#include <strings.h>
+#include <stdlib.h>
+#include <string>
+#include <pthread.h>
+using namespace std;
+
+void *task1(void *);
+std::string getReply(std::string str);
+
+static int connFd;
+
+int main(int argc, char* argv[])
+{
+    int pId, portNo, listenFd;
+    socklen_t len; //store size of the address
+    bool loop = false;
+    struct sockaddr_in svrAdd, clntAdd;
+    
+    pthread_t threadA[3];
+    
+    if (argc < 2)
+    {
+        cerr << "Syntax : ./server <port>" << endl;
+        return 0;
+    }
+    
+    portNo = atoi(argv[1]);
+    
+    if((portNo > 65535) || (portNo < 2000))
+    {
+        cerr << "Please enter a port number between 2000 - 65535" << endl;
+        return 0;
+    }
+    
+    //create socket
+    listenFd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    
+    if(listenFd < 0)
+    {
+        cerr << "Cannot open socket" << endl;
+        return 0;
+    }
+    
+    bzero((char*) &svrAdd, sizeof(svrAdd));
+    
+    svrAdd.sin_family = AF_INET;
+    svrAdd.sin_addr.s_addr = INADDR_ANY;
+    svrAdd.sin_port = htons(portNo);
+    
+    //bind socket
+    if(bind(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd)) < 0)
+    {
+        cerr << "Cannot bind" << endl;
+        return 0;
+    }
+    
+    listen(listenFd, 5);
+    
+    len = sizeof(clntAdd);
+    
+    int noThread = 0;
+
+    while (true)
+    {
+        cout << "Listening" << endl;
+
+        //this is where client connects. svr will hang in this mode until client conn
+        connFd = accept(listenFd, (struct sockaddr *)&clntAdd, &len);
+
+        if (connFd < 0)
+        {
+            cerr << "Cannot accept connection" << endl;
+            return 0;
+        }
+        else
+        {
+            cout << "Connection successful" << endl;
+        }
+        
+        //pthread_create(&threadA[noThread], NULL, task1, NULL); 
+        task1(0);
+        noThread++;
+    }
+    
+    for(int i = 0; i < 3; i++)
+    {
+        pthread_join(threadA[i], NULL);
+    }
+    
+    
+}
+
+void *task1 (void *dummyPt)
+{
+    cout << "Thread No: " << pthread_self() << endl;
+    char test[257];
+    std::string str="";
+    bool loop = false;
+    while(!loop)
+    {    
+        bzero(test, 257);
+        
+        
+        int i=read(connFd, test, 256);
+        str += test;
+        std::cout<<test;
+        
+        if(str.find("\r\n\r\n")!=std::string::npos)
+            break;
+    }
+    std::string reply=getReply(str);
+    write(connFd, reply.c_str(), reply.length());
+    cout << "\nClosing thread and conn" << endl;
+    close(connFd);
+}
+
+std::string getReply(std::string str)
+{
+	return "HTTP/1.1 200 OK\r\n\r\nhello world\r\n\r\n";
+}
