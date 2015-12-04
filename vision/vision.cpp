443c8d7855baac1095d4176f7485a1f83b95a4f9
Citrus Circuits <citrus@localhost.localdomain>
2015-12-04 04:39:42 -0800

    enabled OpenCV

--- vision/vision.cpp
+++ vision/vision.cpp
@@ -1,10 +1,26 @@
 #include "vision.h"
+#include <iostream>
 namespace CitrusVision {
 float x=0, y=0;
 void visionInit() {
+    using namespace cv;
+    cv::VideoCapture cap;
+    cv::Mat m=cv::Mat(100, 100, CV_32F, cv::Scalar(255, 255, 255));
+    cv::namedWindow("webcam", CV_WINDOW_AUTOSIZE);
+    cv::imshow("webcam", m);
+    waitKey(20);
+    std::cout<<"opening webcam (this will take about 10 seconds)\n";
+    if(!cap.open(1)) {
+        std::cout<< "could not open camera";
+        exit(0);
+    }
+    cv::namedWindow("webcam");
     while(true) {
+        cap >> m;
+        cv::imshow("webcam", m);
+        waitKey(20);
         x=1;
         y=2;
     }
 }
-}
+}// namespace CitrusVision
--- vision/vision.h
+++ vision/vision.h
@@ -1,6 +1,7 @@
 #ifndef _VISION_H_
 #define _VISION_H_
 
+#include "opencv2/opencv.hpp"
 namespace CitrusVision {
     extern float x, y;
     void visionInit();
